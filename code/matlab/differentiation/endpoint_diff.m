7341fc399b7e1adc219e85e427f9bdfed13a384b
Andrew Benton <andrew.reed.benton@gmail.com>
2016-08-15 16:26:47 -0400

    Comments

--- code/matlab/differentiation/endpoint_diff.m
+++ code/matlab/differentiation/endpoint_diff.m
@@ -1,12 +1,28 @@
 function [v] = endpoint_diff(f, p, h, points)
 
+    % Endpoint Formulas for Differentiation
+    %
+    % Approximates f'(p), given f and p.
+    %
+    % Error : 3 Point : |f'(x) - f'(x)| <= h^2 / 3 f^(3) (xi)
+    %         5 Point : |f'(x) - f'(x)| <= h^4 / 5 f^(5) (xi)
+    % Notes : Prone to roundoff error, an optimal h != 0 exists
+    %
+    % Arguments:
+    %   f : function
+    %   p : x value to be approximated
+    %   h : step size
+    %   points : points to be used (3 or 5)
+    % Returns:
+    %   v : approximated value of f'(p)
+
     if ~exists('points', 'var')
         points = 5
     end
 
     if points == 3
         % 3 point endpoint formula
-        v = (-3*f(p)+4*f(p+h)-1*f(p+2*h)) / (2*h); 
+        v = (-3*f(p)+4*f(p+h)-1*f(p+2*h)) / (2*h);
     elseif points == 5
         % 5 point endpoint formula
         v = (-25*f(p)+48*f(p+h)-36*f(p+2*h)+16*f(p+3*h)-3*f(p+4*h)) / (12*h);
--- code/matlab/differentiation/midpoint_diff.m
+++ code/matlab/differentiation/midpoint_diff.m
@@ -1,12 +1,28 @@
 function [v] = midpoint_diff(f, p, h, points)
 
+    % Midpoint Formulas for Differentiation
+    %
+    % Approximates f'(p), given f and p.
+    %
+    % Error : 3 Point : |f'(x) - f'(x)| <= h^2 / 6 f^(3) (xi)
+    %         5 Point : |f'(x) - f'(x)| <= h^4 / 30 f^(5) (xi)
+    % Notes : Prone to roundoff error, an optimal h != 0 exists
+    %
+    % Arguments:
+    %   f : function
+    %   p : x value to be approximated
+    %   h : step size
+    %   points : points to be used (3 or 5)
+    % Returns:
+    %   v : approximated value of f'(p)
+
     if ~exists('points', 'var')
         points = 5
     end
 
     if points == 3
         % 3 point midpoint formula
-        v = (f(p+h)-f(p-h)) / (2*h); 
+        v = (f(p+h)-f(p-h)) / (2*h);
     elseif points == 5
         % 5 point midpoint formula
         v = (f(p-2*h)-8*f(p-h)+8*f(p+h)-f(p+2*h)) / (12*h);
